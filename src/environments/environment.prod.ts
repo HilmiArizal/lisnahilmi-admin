77de7dfdc5dcbe6954d1f78d62251c4cc26effde
Hilmi Arizal <hilmi.arizal36@gmail.com>
2021-11-19 13:08:39 +0700

    update-production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  url: {
+    local: 'http://localhost:8000/api/',
+    server: 'https://lisnahilmi.herokuapp.com/api/',
+    image: 'https://lisnahilmi.herokuapp.com/',
+  }
 };
